cde7c2f5180b3fb31714ed00e01dd2afa0a66e57
zKeeni <kromero6880@outlook.com>
2025-12-17 07:58:53 -0500

    fix: corregir la consulta en la vista VW_USUARIO_ROL para usar el campo correcto de rol_id

--- bd/vistaUsuarios.sql
+++ bd/vistaUsuarios.sql
@@ -1,15 +1,19 @@
 CREATE OR REPLACE VIEW VW_USUARIO_ROL AS
-SELECT 
-    u.user_id, 
-    u.user_nombres, 
-    u.user_apellidos, 
-    u.user_username, 
-    u.user_contrasenia, 
+SELECT
+    u.user_id,
+    u.user_nombres,
+    u.user_apellidos,
+    u.user_username,
+    u.user_contrasenia,
     r.rol_nombre,
     u.user_correo,
     u.user_estado
 FROM usuario u
-JOIN usuario_rol ur 
+JOIN usuario_rol ur
     ON u.user_id = ur.user_id
-JOIN rol r 
-    ON r.rol_id = ur.user_rol_id;
+JOIN rol r
+    ON r.rol_id = ur.rol_id
+WHERE u.user_estado = 1
+  AND ur.user_rol_estado = 1;
+
+COMMIT;
